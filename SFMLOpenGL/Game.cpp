a5842df4bd68aac79de26d2fd3dcbfe0acf6b2c3
NiamhAndrews <neand2006@gmail.net>
2019-12-09 18:48:57 +0000

    Added Lines, Line strip, Line loop display

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -51,9 +51,36 @@ void Game::update()
 		glBegin(GL_TRIANGLES);
 		{
 			glColor3f(0.0f, 0.9f, 0.8f);
-			glVertex3f(0.0, 2.0, -5.0);
-			glVertex3f(-2.0, -2.0, -5.0);
-			glVertex3f(2.0, -2.0, -5.0);
+			glVertex3f(0.0, 1.0, -5.0);
+			glColor3f(0.1f, 0.2f, 0.3f);
+			glVertex3f(-1.0, -1.0, -5.0);
+			glColor3f(0.1f, 0.2f, 0.3f);
+			glVertex3f(1.0, -1.0, -5.0);
+
+
+			//Bright Orange
+			//glColor3f(1.0f, 0.3f, 0.0f);
+
+			//Super vivid green
+			//glColor3f(0.4f, 1.0f, 0.0f);
+
+			//Sky blue
+			//glColor3f(0.0f, 1.0f, 1.0f);
+
+			//Bright purple
+			//glColor3f(0.5f, 0.0f, 0.9f);
+
+			//Moave
+			//glColor3f(0.7f, 0.0f, 0.4f);
+
+			//Link green
+
+			//Mint
+			//glColor3f(0.0f, 0.9f, 0.8f);
+			//Deep Navy
+			//glColor3f(0.1f, 0.2f, 0.3f);
+
+			std::cout << "GL Triangles\n\n";
 		}
 		glEnd();
 	}
@@ -63,9 +90,15 @@ void Game::update()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBegin(GL_POINTS);
 		{
-			glColor3f(0.6f, 0.2f, 0.0f);
-			glVertex3f(-2.0, -2.0, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			glVertex3f(0.0, 1.0, -5.0);
+			glVertex3f(-1.0, -1.0, -5.0);
+			glVertex3f(1.0, -1.0, -5.0);
+			glVertex3f(2.0, 1.0, -5.0);
+
+			std::cout << "GL Points\n\n";
 		}
+		glEnd();
 	}
 
 	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Num3)) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad3)))
@@ -73,9 +106,17 @@ void Game::update()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBegin(GL_LINES);
 		{
-			glColor3f(0.6f, 0.2f, 0.0f);
-			
+			glColor3f(0.6f, 0.0f, 0.0f);
+			glVertex3f(0.0, 1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(-1.0, -1.0, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			glVertex3f(1.0, -1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(2.0, 1.0, -5.0);
+			std::cout << "GL Lines\n\n";
 		}
+		glEnd();
 	}
 
 	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Num4)) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad4)))
@@ -83,9 +124,17 @@ void Game::update()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBegin(GL_LINE_STRIP);
 		{
-			glColor3f(0.6f, 0.2f, 0.0f);
-
+			glColor3f(0.6f, 0.0f, 0.0f);
+			glVertex3f(0.0, 1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(-1.0, -1.0, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			glVertex3f(1.0, -1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(2.0, 1.0, -5.0);
+			std::cout << "GL Line strip\n\n";
 		}
+		glEnd();
 	}
 
 	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Num5)) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad5)))
@@ -93,9 +142,17 @@ void Game::update()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBegin(GL_LINE_LOOP);
 		{
-			glColor3f(0.6f, 0.2f, 0.0f);
-
+			glColor3f(0.6f, 0.0f, 0.0f);
+			glVertex3f(0.0, 1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(-1.0, -1.0, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			glVertex3f(1.0, -1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(2.0, 1.0, -5.0);
+			std::cout << "GL Line loop\n\n";
 		}
+		glEnd();
 	}
 
 	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Num6)) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad6)))
@@ -103,9 +160,19 @@ void Game::update()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBegin(GL_TRIANGLE_STRIP);
 		{
-			glColor3f(0.6f, 0.2f, 0.0f);
-
+			glColor3f(0.6f, 0.0f, 0.0f);
+			glVertex3f(-2.0, -1.0, -5.0);//causing shape point overlap
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(-1.0, 1.0, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			glVertex3f(0.0, -1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(1.0, 1.0, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			glVertex3f(2.0, -1.0, -5.0);
+			std::cout << "GL Triangle Strip\n\n";
 		}
+		glEnd();
 	}
 
 	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Num7)) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad7)))
@@ -113,9 +180,17 @@ void Game::update()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBegin(GL_TRIANGLE_FAN);
 		{
-			glColor3f(0.6f, 0.2f, 0.0f);
-
+			glColor3f(0.6f, 0.0f, 0.0f);
+			glVertex3f(0.0, 1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(-1.0, -1.0, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			glVertex3f(1.0, -1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(2.0, 1.0, -5.0);
+			std::cout << "GL Triangle Fan\n\n";
 		}
+		glEnd();
 	}
 
 	if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Num8)) || (sf::Keyboard::isKeyPressed(sf::Keyboard::Numpad8)))
@@ -123,9 +198,17 @@ void Game::update()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBegin(GL_QUADS);
 		{
-			glColor3f(0.6f, 0.2f, 0.0f);
-
+			glColor3f(0.6f, 0.0f, 0.0f);
+			glVertex3f(0.0, 1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(-1.0, -1.0, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			glVertex3f(1.0, -1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(2.0, 1.0, -5.0);
+			std::cout << "GL Quads\n\n";
 		}
+		glEnd();
 	}
 
 
@@ -134,9 +217,22 @@ void Game::update()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBegin(GL_QUAD_STRIP);
 		{
-			glColor3f(0.6f, 0.2f, 0.0f);
-
+			glColor3f(0.6f, 0.0f, 0.0f);
+			glVertex3f(-2.0f, -1.0f, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(-1.0f, 1.0f, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			glVertex3f(0.0f, -1.0f, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(1.0f, 1.0f, -5.0);
+			//glColor3f(0.4f, 1.0f, 0.0f);
+			glColor3f(0.0f, 0.1f, 0.9f);
+			glVertex3f(2.0f, 1.0f, -5.0);
+			glColor3f(0.0f, 0.5f, 0.5f);
+			glVertex3f(2.0f, -2.0f, -5.0);
+			std::cout << "GL Quad Strip\n\n";
 		}
+		glEnd();
 	}
 
 
@@ -145,9 +241,19 @@ void Game::update()
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glBegin(GL_POLYGON);
 		{
-			glColor3f(0.6f, 0.2f, 0.0f);
-
+			glColor3f(0.6f, 0.0f, 0.0f);
+			glVertex3f(-2.0, -1.0, -5.0);//causing shape point overlap
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(-0.5, 1.0, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			//glVertex3f(0.0, -1.0, -5.0);
+			glColor3f(1.0f, 0.3f, 0.0f);
+			glVertex3f(0.5, 1.0, -5.0);
+			glColor3f(0.4f, 1.0f, 0.0f);
+			glVertex3f(2.0, -1.0, -5.0);
+			std::cout << "GL Polygon\n\n";
 		}
+		glEnd();
 	}
 
 
